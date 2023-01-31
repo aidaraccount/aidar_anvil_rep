14cee63299ca9f988e397fe17b6db4639d53952a
janek-meyn@web.de <janek-meyn@web.de>
2023-01-31 10:23:54 +0000

    Created C_Rating.RowTemplate1; Edited C_Rating

--- client_code/C_Rating/RowTemplate1/__init__.py
+++ client_code/C_Rating/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import C_RatingTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,16 +6,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class C_Rating(C_RatingTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    global user
-    user = anvil.users.get_user()
-    
-    global cur_model_id
-    cur_model_id = anvil.server.call('GetModelID',  user["user_id"])
-
-    self.data_ratings.items = anvil.server.call('GetRatings', cur_model_id)
