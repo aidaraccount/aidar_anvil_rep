c8a0ff90e566345875d62085892db996c86dd8d0
janek-meyn@web.de <janek-meyn@web.de>
2023-08-16 14:36:34 +0000

    Edited C_Investigate

--- client_code/C_Investigate/__init__.py
+++ client_code/C_Investigate/__init__.py
@@ -27,7 +27,7 @@ class C_Investigate(C_InvestigateTemplate):
   
   def refresh_sug(self, **event_args):
     print(f'Refresh Sug - Start {datetime.datetime.now()}', flush=True)
-    sug = json.loads(anvil.server.call('get_suggestion', cur_model_id, 'Free'))
+    sug = json.loads(anvil.server.call('get_suggestion', cur_model_id, 'Inspect'))
 
     if sug["Status"] == 'Empty Model!':
       alert(title='Train you Model..',
