124f21a17d6e1c2c3c1022b345d754ccae351e9f
janek-meyn@web.de <janek-meyn@web.de>
2023-08-30 11:08:37 +0000

    Created Imprint; Edited Main_Out

--- client_code/Imprint/__init__.py
+++ client_code/Imprint/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Main_OutTemplate
+from ._anvil_designer import ImprintTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,29 +7,28 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import datetime
 from ..Main_In import Main_In
-from ..Imprint import Imprint
 
-class Main_Out(Main_OutTemplate):
+class Imprint(ImprintTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+
     # Any code you write here will run before the form opens.
     check_log_status(self)
-    
+
   def link_login_click(self, **event_args):
     print(f'Login Click - Start {datetime.datetime.now()}', flush=True)
     anvil.users.login_with_form(allow_cancel=True, remember_by_default=True)
     check_log_status(self)
     user = anvil.users.get_user()
-    if (user != None):      
+    if (user != None):
       anvil.server.call('check_user_presence', mail=user['email'])
       open_form('Main_In', temp_artist_id = None, user_id = user["user_id"])
-      
+
   def link_logout_click(self, **event_args):
     anvil.users.logout()
     check_log_status(self)
-  
+
   def button_signup_click(self, **event_args):
     anvil.users.signup_with_form(allow_cancel=True)
 
@@ -39,22 +38,8 @@ class Main_Out(Main_OutTemplate):
       anvil.server.call('check_user_presence', mail=user['email'])
       open_form('Main_In', temp_artist_id = None, user_id = user["user_id"])
 
-  def linkedin_click(self, **event_args):
-    self.linkedin_link.url = 'https://www.linkedin.com/company/aidar-insights/'
-
-  def instagram_click(self, **event_args):
-    self.instagram_link.url = 'https://www.instagram.com/aidar_insights/'
-
-  def call2action_text_box_click(self, **event_args):
-    status = anvil.server.call('add_waiter', mail=self.call2action_text_box.text)
-    if status == 'Success!':
-      alert(title='Thanks for joining our Waiting List!',
-            content='We appreciate your interest in our product and will get back to you as soon as we have the capacity available.\n\nBest regards\nYour AIDAR Team')
-    elif status == 'Error':
-      alert(title='Error..', content='Apologies for the inconvenience caused.\n\nIf the error persists, kindly reach out to us via email at info@aidar.ai.\n\nThank you,\nYour AIDAR Team')
-
-  def imprint_link_click(self, **event_args):
-    open_form('Imprint')
+  def link_main_click(self, **event_args):
+    open_form('Main_Out')
 
 
 def check_log_status(self, **event_args):
@@ -69,4 +54,3 @@ def check_log_status(self, **event_args):
     #self.link_register.visible = False
     self.link_logout.visible = True
     self.link_investigate.visible = True
-  
