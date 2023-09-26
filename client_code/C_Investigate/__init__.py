7727c0ad01e3193615c12d00d762e491000b7466
janek-meyn@web.de <janek-meyn@web.de>
2023-09-26 07:48:00 +0000

    Created icons/+1.png, icons/+2.png, icons/-1.png, icons/-2.png and 2 more; Edited C_Investigate

--- client_code/C_Investigate/__init__.py
+++ client_code/C_Investigate/__init__.py
@@ -25,6 +25,9 @@ class C_Investigate(C_InvestigateTemplate):
     self.plot_popularity.visible = False
     self.plot_followers.visible = False
     self.data_grid_releases.visible = False
+
+    self.artist_popularity_lat.icon = '_/theme/icons/+2.png'
+    self.artist_follower_lat.icon = '_/theme/icons/+1.png'
     self.refresh_sug(temp_artist_id)
 
 
