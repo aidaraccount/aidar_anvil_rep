26abde21fe30fb56a9a87d07a30d9066ebdf4f78
janek-meyn@web.de <janek-meyn@web.de>
2024-04-09 12:20:10 +0000

    Edited C_Investigate

--- client_code/C_Investigate/__init__.py
+++ client_code/C_Investigate/__init__.py
@@ -155,7 +155,7 @@ class C_Investigate(C_InvestigateTemplate):
       if sug["Countries"] == 'None': self.countries.text = '-'
       else: self.countries.text = sug["Countries"]
       
-      self.c_web_player.html = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/' + spotify_artist_id + '?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      self.c_web_player.html = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/' + spotify_artist_id + '?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
   
       # Musical Features
       if sug["AvgDuration"] == 'None': f1 = '-'
