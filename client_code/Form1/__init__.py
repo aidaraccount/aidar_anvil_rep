89b7ffd4f634209e20f1db6d79db5daf602f4aa4
janek-meyn@web.de <janek-meyn@web.de>
2024-04-09 15:42:51 +0000

    Created Form1; Edited theme.css, settings, dashboard.html and C_Discover

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import C_DiscoverTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,34 +6,13 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class C_Discover(C_DiscoverTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-    self.feature_1.value.text = 'Value 1'
-    self.feature_1.header.text = 'Header 1'
-    self.feature_1.width = 200
-
-    self.feature_2.value.text = 'Value 2'
-    self.feature_2.header.text = 'Header 2'
-    self.feature_2.width = 400
     
-    self.feature_3.value.text = 'Value 3'
-    self.feature_3.header.text = 'Header 3'
-    self.feature_3.width = 400
-    self.feature_3.visible = False
-    
-    self.feature_4.value.text = 'Value 4'
-    self.feature_4.header.text = 'Header 4'
-    self.feature_4.width = 400
-    
-    self.feature_double_1.value.text = 'Value Double 4'
-    self.feature_double_1.header.text = 'Header Double 4'
-    self.feature_double_1.width = 200
-
     # Section 2: Spotify Web-Player
     spotify_artist_id = '5tHOqv4SnPS6Dv4rZuUOxP'
     self.c_web_player.html = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/' + spotify_artist_id + '?utm_source=generator&theme=0&autoplay=true" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
