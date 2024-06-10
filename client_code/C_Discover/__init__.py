f65fe31afdd5fe57bef8c53f8111f9c822a62dce
janek-meyn@web.de <janek-meyn@web.de>
2024-06-10 18:52:15 +0000

    Edited settings and C_Discover

--- client_code/C_Discover/__init__.py
+++ client_code/C_Discover/__init__.py
@@ -13,7 +13,7 @@ import plotly.graph_objects as go
 
 
 class C_Discover(C_DiscoverTemplate):
-  def __init__(self, model_id=2, temp_artist_id=2, **properties):
+  def __init__(self, model_id, temp_artist_id, **properties):
     print(f"{datetime.now()}: C_Discover - __init__ - 1", flush=True)
       
     # Set Form properties and Data Bindings.
