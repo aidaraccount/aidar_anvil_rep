54deb5bbd04dce74923ba21e3a1831d9e84d333c
janek-meyn@web.de <janek-meyn@web.de>
2024-06-12 07:32:25 +0000

    Edited theme.css and Main_In

--- client_code/Main_In/__init__.py
+++ client_code/Main_In/__init__.py
@@ -43,7 +43,7 @@ class Main_In(Main_InTemplate):
     else:
       if model_id is None:
         self.model_id = anvil.server.call('get_model_id',  user["user_id"])
-        anvil.server.call('update_model_usage', self.model_id)
+        # anvil.server.call('update_model_usage', self.model_id)
       else:
         self.model_id = model_id
     
