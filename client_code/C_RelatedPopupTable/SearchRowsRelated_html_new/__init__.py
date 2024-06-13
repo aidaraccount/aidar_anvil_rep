0a7a055c0a69f7bbbac33b8a16ce5a0e7475cf57
janek-meyn@web.de <janek-meyn@web.de>
2024-06-13 14:06:37 +0000

    Edited C_RelatedPopupTable, C_RelatedPopupTable.SearchRowsRelated_html_new, settings, theme.css and 1 more

--- client_code/C_RelatedPopupTable/SearchRowsRelated_html_new/__init__.py
+++ client_code/C_RelatedPopupTable/SearchRowsRelated_html_new/__init__.py
@@ -14,17 +14,15 @@ class SearchRowsRelated_html_new(SearchRowsRelated_html_newTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
     global user
     user = anvil.users.get_user()
     global model_id
     model_id = self.item["ModelID"]
 
-  # BUTTONS
+  # BUTTONS 
   def button_related_click(self, **event_args):
-    self.parent.parent.parent.close_alert()
-    
+    self.parent.parent.parent.close_alert()    
     open_form(
       "Main_In",
       model_id=model_id,
