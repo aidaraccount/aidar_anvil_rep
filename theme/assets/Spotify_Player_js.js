8265505ebbe841cbfdc29f2ebc7c08b3bd80eb33
janek-meyn@web.de <janek-meyn@web.de>
2024-08-28 09:14:50 +0000

    Edited theme.css, Discover, theme/parameters.yaml and Spotify_Player_js.js

--- theme/assets/Spotify_Player_js.js
+++ theme/assets/Spotify_Player_js.js
@@ -3,13 +3,15 @@ var controller;
 
 function createOrUpdateSpotifyPlayer(trackOrArtist, artistSpotifyID) {
   const element = document.querySelector('.anvil-role-spotify-footer-class #embed-iframe');
-    
+  const autoplaybutton = document.querySelector('.anvil-role-autoplay-toggle-button')
+  console.log("THIS IS THE ELEMENT:", element)
+  console.log("THIS IS THE AUTOPLAY BUTTON ELEMENT:", autoplaybutton)
+  
   if (!element) {
     console.error("ERROR MESSAGE: Embed iframe element not found.")
     return;
   }
-    
-  console.log("THIS IS THE ELEMENT:", element);
+  
   console.log(artistSpotifyID);
   
   const options = {
