3974b42711f07f191b0879228f25e47960e6b107
janek-meyn@web.de <janek-meyn@web.de>
2024-10-07 13:56:35 +0000

    Edited Main_Out_New

--- client_code/Main_Out_New/__init__.py
+++ client_code/Main_Out_New/__init__.py
@@ -5,11 +5,62 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import datetime
+import re
+
+from anvil_extras import routing
+from anvil.js.window import location
+from ..nav import click_link, click_button, logout, save_var, load_var
 
 
 class Main_Out_New(Main_Out_NewTemplate):
-  def __init__(self, **properties):
+  def __init__(self, **properties):    
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    global user
+    user = None
+    
+    model_id = load_var("model_id")
+    print(f"Main_Out model_id: {model_id}")
+    
     # Any code you write here will run before the form opens.
+    # check_log_status(self)
+
+  def button_login_click(self, **event_args):
+    # try:
+      user = anvil.users.login_with_email(self.login_email.text,self.login_pw.text)
+      # check_log_status(self)
+      user = anvil.users.get_user()
+      save_var("user", user)
+      if user is not None:
+        # try:
+          anvil.server.call("server_transfer_user_id")
+          if user["user_id"] is not None:
+            save_var("user_id", user["user_id"])
+            save_var("model_id", anvil.server.call('get_model_id',  user["user_id"]))
+          open_form("Main_In")
+        
+          if location.hash == '':
+            routing.set_url_hash('home', load_from_cache=False)
+          elif location.hash[:8] == '#artists':
+            routing.set_url_hash(location.hash, load_from_cache=False)
+
+# def check_log_status(self, **event_args):
+#   if anvil.users.get_user() is None:    
+#     self.link_login.visible = True
+#     self.link_register.visible = True
+#     self.link_logout.visible = False
+#     self.link_home.visible = False
+    
+#   else:
+#     self.link_login.visible = False
+#     self.link_register.visible = False
+#     self.link_logout.visible = True
+#     self.link_home.visible = True
+    
+#   if self.updates_sign.visible is True:
+#     self.link_login.visible = False
+#     self.link_register.visible = False
+#     self.link_logout.visible = False
+#     self.link_home.visible = False
