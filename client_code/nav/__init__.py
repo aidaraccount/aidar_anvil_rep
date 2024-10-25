585c5b12cbca2549b58cc4a48f75ac3cabe291db
janek-meyn@web.de <janek-meyn@web.de>
2024-10-25 13:30:55 +0000

    Edited nav

--- client_code/nav/__init__.py
+++ client_code/nav/__init__.py
@@ -12,23 +12,24 @@ from anvil import get_open_form
 
 def click_link(element, target, event_args):
   routing.clear_cache()
-  if event_args['keys']['ctrl'] is True:
-    element.url = f"{anvil.server.get_app_origin()}/#{target}"
-  else:
-    element.url = ''
-    routing.set_url_hash(target, load_from_cache=False)
+  print("event_args:", event_args)
+  # if event_args['keys']['ctrl'] is True:
+  #   element.url = f"{anvil.server.get_app_origin()}/#{target}"
+  # else:
+  element.url = ''
+  routing.set_url_hash(target, load_from_cache=False)
   get_open_form().reset_nav_backgrounds()
 
 
 def click_button(target, event_args):
   routing.clear_cache()
-  if event_args != {}:
-    if event_args['keys']['ctrl'] is True:
-      anvil.js.window.open(f"{anvil.server.get_app_origin()}/#{target}", '_blank')
-    else:
-      routing.set_url_hash(target, load_from_cache=False)
-  else:
-    routing.set_url_hash(target, load_from_cache=False)
+  # if event_args != {}:
+  #   if event_args['keys']['ctrl'] is True:
+  #     anvil.js.window.open(f"{anvil.server.get_app_origin()}/#{target}", '_blank')
+  #   else:
+  #     routing.set_url_hash(target, load_from_cache=False)
+  # else:
+  routing.set_url_hash(target, load_from_cache=False)
   get_open_form().reset_nav_backgrounds()
 
 
