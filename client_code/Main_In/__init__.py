10b99c27ccc69a7e0518ddc8ccac9e7d0062bd66
janek-meyn@web.de <janek-meyn@web.de>
2024-11-22 13:19:19 +0000

    Edited dashboard.html and Main_In

--- client_code/Main_In/__init__.py
+++ client_code/Main_In/__init__.py
@@ -85,6 +85,7 @@ class Main_In(Main_InTemplate):
             
       if self.model_id is None:
         routing.set_url_hash('no_model', load_from_cache=False)
+        # self.change_nav_visibility(status=False)
         self.change_nav_visibility(status=False)
         self.SearchBar.visible = False
 
@@ -258,7 +259,8 @@ class Main_In(Main_InTemplate):
     self.column_panel_nav.visible = status
     
     # self.link_models.visible = True
-    self.call_js("navbar_noModel_noSubs", status)
+    # self.call_js("navbar_noModel_noSubs", status)
+    self.call_js("toggleSidebar")
   
   #----------------------------------------------------------------------------------------------
   # HOME
