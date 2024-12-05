433098bdf78dfb35b720e15a183a07b9cb28b360
janek-meyn@web.de <janek-meyn@web.de>
2024-12-05 10:48:42 +0000

    Created C_FeedbackForm

---
+++ client_code/C_FeedbackForm/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import C_FeedbackFormTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class C_FeedbackForm(C_FeedbackFormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
