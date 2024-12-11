56c03eb257933125a07a5be9699ccae79494ac91
janek-meyn@web.de <janek-meyn@web.de>
2024-12-11 20:58:12 +0000

    Created Observe_Listen.RepeatingPanel_Artists.RepeatingPanel_Tracks and Observe_Listen.RepeatingPanel_Artists; Edited Observe_Listen

---
+++ client_code/Observe_Listen/RepeatingPanel_Artists/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import RepeatingPanel_ArtistsTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class RepeatingPanel_Artists(RepeatingPanel_ArtistsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    print(self.item["tracks"])
+    self.repeating_panel_tracks.items = self.item["tracks"]
--- client_code/Observe_Listen/__init__.py
+++ client_code/Observe_Listen/__init__.py
@@ -44,17 +44,47 @@ class Observe_Listen(Observe_ListenTemplate):
       # load first notification
       self.no_notifications.visible = False
       self.get_notifications(notification["notification_id"])
+      self.get_observe_tracks(notification["notification_id"])
     else:
       self.flow_panel.visible = False
       self.data_grid.visible = False
       self.no_notifications.visible = True
 
   
-  # GET TABLE DATA
+  # GET NOTIFICATION DETAILS
   def get_notifications(self, notification_id, **event_args):
     self.repeating_panel_email.items = [item for item in self.notifications if item["notification_id"] == notification_id]
     self.data_grid.visible = True
 
+  # GET PLAYLIST DETAILS
+  def get_observe_tracks(self, notification_id, **event_args):
+    
+    notification = [item for item in self.notifications if item["notification_id"] == notification_id][0]
+    
+    observed_tracks = anvil.server.call('get_observed_tracks', 
+                                        user["user_id"],
+                                        notification["model_ids"],
+                                        notification["metric"],
+                                        notification["rated"],
+                                        notification["watchlist"],
+                                        notification["min_grow_fit"],
+                                        notification["release_days"],
+                                        notification["no_artists"],
+                                        notification["song_selection_2"]
+                                        )
+
+    print(observed_tracks)
+    
+    self.repeating_panel_artists.items = observed_tracks
+    self.repeating_panel_artists.visible = True
+
+
+  # GET DISCOVER DETAILS
+  # .....
+  # .....
+  # .....
+  
+  
   def add_spotify_playlist_click(self, **event_args):
     # get a trained model to activate it at the beginning
     models = json.loads(anvil.server.call("get_model_ids", user["user_id"]))
@@ -100,11 +130,13 @@ class Observe_Listen(Observe_ListenTemplate):
 
     # update the notifications table
     self.get_notifications(notification_id)
+    self.get_observe_tracks(notification_id)
 
   
   # BASE FUNCTIONS FOR LINK EVENTS
   def create_click_notification_handler(self, notification_id):
     def handler(**event_args):
       self.get_notifications(notification_id)
+      self.get_observe_tracks(notification_id)
     return handler
     
