f8d6c204620f22166d613811b9bc05dc709ed7b8
janek-meyn@web.de <janek-meyn@web.de>
2024-12-12 20:09:47 +0000

    Edited ServerModule1 and Discover

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,7 +3,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-import numpy as 
+import numpy as np
 from anvil import get_open_form
 
 
