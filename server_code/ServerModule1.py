9227aa93afd7781ce0c8072b8335cc8187ac1cda
janek-meyn@web.de <janek-meyn@web.de>
2024-12-18 12:17:46 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -4,7 +4,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import numpy as np
-from anvil import get_open_form
+# from anvil import get_open_form
 
 
 # This is a server module. It runs on the Anvil server,
