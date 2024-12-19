1950f659589d1a26ed1fea841a0f197ea4ec3f57
janek-meyn@web.de <janek-meyn@web.de>
2024-12-19 15:01:03 +0000

    Edited theme.css, Observe_Listen.RepeatingPanel_Artists.RepeatingPanel_Tracks, theme/parameters.yaml and Observe_Listen.RepeatingPanel_Artists

--- client_code/Observe_Listen/RepeatingPanel_Artists/RepeatingPanel_Tracks/__init__.py
+++ client_code/Observe_Listen/RepeatingPanel_Artists/RepeatingPanel_Tracks/__init__.py
@@ -12,7 +12,7 @@ class RepeatingPanel_Tracks(RepeatingPanel_TracksTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
 
     # set album picture    
