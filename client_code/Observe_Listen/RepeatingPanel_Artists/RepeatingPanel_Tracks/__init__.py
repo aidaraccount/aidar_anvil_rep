a4a56725f7d327034e5fe732a714fc90b37b14fb
janek-meyn@web.de <janek-meyn@web.de>
2025-01-10 13:05:32 +0000

    Edited theme_1_base.css, settings and Observe_Listen.RepeatingPanel_Artists.RepeatingPanel_Tracks

--- client_code/Observe_Listen/RepeatingPanel_Artists/RepeatingPanel_Tracks/__init__.py
+++ client_code/Observe_Listen/RepeatingPanel_Artists/RepeatingPanel_Tracks/__init__.py
@@ -8,6 +8,7 @@ from anvil.tables import app_tables
 
 from ....nav import click_link, click_button, load_var, save_var
 
+
 class RepeatingPanel_Tracks(RepeatingPanel_TracksTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -17,7 +18,7 @@ class RepeatingPanel_Tracks(RepeatingPanel_TracksTemplate):
     global user
     user = anvil.users.get_user()
 
-    # self.play_button_copy.visible = False
+    self.play_button_copy.visible = False
     
     # set album picture    
     if self.item["album_picture_url"] is not None:
@@ -55,10 +56,30 @@ class RepeatingPanel_Tracks(RepeatingPanel_TracksTemplate):
     anvil.server.call('sent_push_over',  'Observe_Listen', f'User {user["user_id"]}: track click')
 
   def album_img_copy_mouse_enter(self, x, y, **event_args):
-    # self.play_button_copy.visible = True
-    print("in!")
+    self.play_button_copy.visible = True
+    # print(f"in: {x}/{y}")
+    save_var('x_in', x)
+    save_var('y_in', y)
 
   def album_img_copy_mouse_leave(self, x, y, **event_args):
-    # self.play_button_copy.visible = False
-    print("out!")
-    print(event_args)
+    x_in = float(load_var('x_in'))
+    y_in = float(load_var('y_in'))
+    
+    # if x_in == 0 & 3 < x < 47: 
+    #   # left in
+    #   pass
+    # elif x_in == 49 & 3 < x < 47: 
+    #   # right in
+    #   pass
+    # elif y_in < 1 & 3 < y < 47: 
+    #   # top in
+    #   pass
+    # elif y_in > 49 & 3 < y < 47: 
+    #   # bottom in
+    #   pass
+    # else:
+    #   self.play_button_copy.visible = False
+
+    # if x == -1 or 
+    # print(f"in: {x_in}/{y_in} | out: {x}/{y}")
+    print(f"out: {x}/{y}")
