312835b71a13513a5e7b2e73d2fee81c2fe2fe97
janek-meyn@web.de <janek-meyn@web.de>
2025-01-15 13:45:48 +0000

    Created MainOut_Register; Edited C_ForgotPasswordPopup and MainOut

--- client_code/MainOut_Register/__init__.py
+++ client_code/MainOut_Register/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import MainOutTemplate
+from ._anvil_designer import MainOut_RegisterTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -14,68 +14,54 @@ from ..nav import click_link, click_button, logout, save_var, load_var
 from ..C_ForgotPasswordPopup import C_ForgotPasswordPopup
 
 
-@routing.route('login', title='Login')
-class MainOut(MainOutTemplate):
+@routing.route("register", title="Register")
+class MainOut_Register(MainOut_RegisterTemplate):
   def __init__(self, **properties):
-    
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    global user
-    user = None
-    
-    model_id = load_var("model_id")
-    print(f"MainOut model_id: {model_id}")
-    
     # Any code you write here will run before the form opens.
     pass
-    
 
-  def button_login_click(self, **event_args):
+  
+  def button_register_click(self, **event_args):
     """Triggered when the user clicks the login button"""
     # Get values from the form
     email = self.login_email.text
     password = self.login_pw.text
     remember_me = self.remember_me_checkbox.checked
-    
+
     try:
       # Log in user with the "Remember me" option
       user = anvil.users.login_with_email(email, password, remember=remember_me)
-      
+
       # user = anvil.users.get_user()
-      
+
       save_var("user", user)
       if user is not None:
-          anvil.server.call("server_transfer_user_id")
-        
-          # Save user_id and model_id to session storage
-          if user["user_id"] is not None:
-            save_var("user_id", user["user_id"])
-            save_var("model_id", anvil.server.call('get_model_id',  user["user_id"]))
+        anvil.server.call("server_transfer_user_id")
 
-          # Navigate to main page after successful login
-          open_form("MainIn")
-        
-          if location.hash == '':
-            routing.set_url_hash('home', load_from_cache=False)
-          elif location.hash[:8] == '#artists':
-            routing.set_url_hash(location.hash, load_from_cache=False)
-            
+        # Save user_id and model_id to session storage
+        if user["user_id"] is not None:
+          save_var("user_id", user["user_id"])
+          save_var("model_id", anvil.server.call("get_model_id", user["user_id"]))
+
+        # Navigate to main page after successful login
+        open_form("MainIn")
+
+        if location.hash == "":
+          routing.set_url_hash("home", load_from_cache=False)
+        elif location.hash[:8] == "#artists":
+          routing.set_url_hash(location.hash, load_from_cache=False)
+
     except anvil.users.AuthenticationFailed:
       alert(
-          "Please check your credentials.",
-          title="Login Failed.",
-          large=False,
-          buttons=[("Go Back", True)],
-          role=["forgot-password-success", "remove-focus"]
-        )
-
-  def link_forgot_password_click(self, **event_args):
-    """Triggered when the user clicks 'Forgot your password?'"""
-    alert(
-      content=C_ForgotPasswordPopup(),
-      buttons=[]
-    )
+        "Please check your credentials.",
+        title="Login Failed.",
+        large=False,
+        buttons=[("Go Back", True)],
+        role=["forgot-password-success", "remove-focus"],
+      )
 
   def login_email_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
