bf88025707c711207b8df7ba44f45613d5255a57
janek-meyn@web.de <janek-meyn@web.de>
2025-02-17 10:17:09 +0000

    Edited theme_2_buttons.css, C_Short and theme_4_page_indiv.css

--- client_code/C_Short/__init__.py
+++ client_code/C_Short/__init__.py
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-from ..nav import click_link
+from ..nav import click_link, click_button
 
 
 class C_Short(C_ShortTemplate):
@@ -14,7 +14,7 @@ class C_Short(C_ShortTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    print(data)
+    # print(data["artist_id"], data["watchlist_id"])
     
     views = '-' if data["views"] is None else f'{data["views"]:,}'
     likes = '-' if data["likes"] is None else f'{data["likes"]:,}'
@@ -25,9 +25,9 @@ class C_Short(C_ShortTemplate):
     <div class="masonry-item">
       <div anvil-role="social-name" class="social-name" anvil-slot="name-slot">
         <div anvil-if-slot-empty="name-slot">{data["name"]}</div>
-      </div>
-      <div anvil-role="social-wl-button" class="social-wl-button" anvil-slot="wl-button-slot">
-        <div anvil-if-slot-empty="wl-button-slot">{data["watchlist_id"]}</div>
+        <div anvil-role="social-wl-button" class="social-wl-button" anvil-slot="wl-button-slot">
+          <div anvil-if-slot-empty="wl-button-slot">{data["watchlist_id"]}</div>
+        </div>
       </div>
       <p anvil-role="social-date" class="label-text social-date">{data["created_date"]}</p>
       <iframe src="{data["external_url"]}/embed/?omitscript=true&hidecaption=true"
@@ -50,9 +50,19 @@ class C_Short(C_ShortTemplate):
     )
     self.add_component(link, slot="name-slot")
 
+    button = Button(icon='fa:address-card-o', role=['icon-button-disabled-small'])
+    button.set_event_handler(
+      "click", self.create_button_click_handler(data["artist_id"], data["watchlist_id"])
+    )
+    self.add_component(button, slot="wl-button-slot")
+
   
   def create_link_click_handler(self, artist_id, link):
     def handler(**event_args):
       click_link(link, f"artists?artist_id={artist_id}", event_args)
+    return handler
 
+  def create_button_click_handler(self, artist_id, watchlist_id, **event_args):
+    def handler(**event_args):
+      click_button(f'watchlist_details?watchlist_id={watchlist_id}&artist_id={artist_id}', event_args)
     return handler
