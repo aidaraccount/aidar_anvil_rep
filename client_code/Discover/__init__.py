a960a13fb234a569414bac794e0159a5573b7b48
janek-meyn@web.de <janek-meyn@web.de>
2025-03-26 19:30:17 +0000

    Edited Discover

--- client_code/Discover/__init__.py
+++ client_code/Discover/__init__.py
@@ -2303,9 +2303,12 @@ class Discover(DiscoverTemplate):
     save_var('autoPlayStatus', self.autoplay_button.icon)
     
   def show_milestone_alert(self, milestone):
-    # Show a congratulatory alert when a user reaches a milestone.
-    alert(
-        content=C_ProgressMessage(self.model_id, milestone),
-        buttons=[],
-        role=["progress-message","remove-focus"]
-    )
+    # Show a congratulatory alert when a user reaches a milestone and it wasn't displayed before.
+    displayed_before = anvil.server.call('get_progress_msg_status', self.model_id_id, milestone)
+    if displayed_before is False:
+      anvil.server.call('update_progress_msg_status', self.model_id_id, milestone)
+      alert(
+          content=C_ProgressMessage(self.model_id, milestone),
+          buttons=[],
+          role=["progress-message","remove-focus"]
+      )
