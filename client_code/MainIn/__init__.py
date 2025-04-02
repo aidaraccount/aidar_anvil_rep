2e68fe89bebf00d4fed365a0c27e585bb74613ca
janek-meyn@web.de <janek-meyn@web.de>
2025-04-02 10:17:43 +0000

    Edited C_Discover, C_LevelOfPopularity, CreateWatchlist, Discover and 8 more

--- client_code/MainIn/__init__.py
+++ client_code/MainIn/__init__.py
@@ -86,7 +86,6 @@ class MainIn(MainInTemplate):
             
       if self.model_id is None:
         routing.set_url_hash('no_model', load_from_cache=False)
-        self.change_nav_visibility(status=True)
         self.SearchBar.visible = True
 
       #print(f"{datetime.datetime.now()}: MainIn - link_login_click - 4", flush=True)
@@ -272,22 +271,6 @@ class MainIn(MainInTemplate):
             if int(component.tag) == int(model_id):
               component.background = "theme:Brown"
   
-  def change_nav_visibility(self, status, **event_args):
-    self.image_1.visible = status
-    self.link_home.visible = status
-    self.linear_panel_discover.visible = status
-    self.link_discover.visible = status
-    # self.link_discover_ai.visible = status
-    # self.link_discover_rel.visible = status
-    self.link_observe.visible = status
-    self.nav_watchlists.visible = status
-    self.column_panel_nav_wl.visible = status
-    self.linear_panel_monitor.visible = status
-    # self.link_monitor_funnel.visible = status
-    # self.link_monitor_dev.visible = status
-    self.column_panel_nav.visible = status
-
-    # self.link_models.visible = True
   #----------------------------------------------------------------------------------------------
   # HOME
   def link_home_click(self, **event_args):
