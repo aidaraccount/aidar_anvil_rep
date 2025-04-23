0b47b2c1fb7bb4f69c1a3572c9f524c4cbfb9241
janek-meyn@web.de <janek-meyn@web.de>
2025-04-23 17:45:30 +0000

    Created C_SubscriptionPlan; Edited Settings

---
+++ client_code/C_SubscriptionPlan/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import C_SubscriptionPlanTemplate
+from anvil import *
+import anvil.server
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class C_SubscriptionPlan(C_SubscriptionPlanTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
