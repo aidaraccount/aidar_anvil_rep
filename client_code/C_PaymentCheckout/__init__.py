8dfed5725cbfd5971c624833adeb1919166a02a0
janek-meyn@web.de <janek-meyn@web.de>
2025-04-24 14:11:40 +0000

    Created C_PaymentCheckout and Settings/C_PaymentCheckout.css; Edited theme.css

---
+++ client_code/C_PaymentCheckout/__init__.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import C_PaymentCheckoutTemplate
+from anvil import *
+import anvil.server
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class C_PaymentCheckout(C_PaymentCheckoutTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    global user
+    user = anvil.users.get_user()
+    
+    # Get the Stripe SetupIntent client_secret from the server
+    client_secret = anvil.server.call('create_setup_intent')
+    self.html = f"""
+    """
