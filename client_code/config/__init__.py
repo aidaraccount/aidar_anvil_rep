48e22440596b79e8ba0e1decef9b9d18b0f3bdb4
janek-meyn@web.de <janek-meyn@web.de>
2025-07-02 18:17:30 +0000

    Edited config

--- client_code/config/__init__.py
+++ client_code/config/__init__.py
@@ -12,7 +12,7 @@ _config_cache = {}
 
 # --- 3. WEBSOCKET CONFIGURATION ---
 # Simple flag to determine which WebSocket server to use
-USE_LOCAL_WEBSOCKET = False
+USE_LOCAL_WEBSOCKET = True
 
 # WebSocket server URLs
 LOCAL_WEBSOCKET_URL = "ws://localhost:8000"
@@ -31,6 +31,7 @@ def set_websocket_environment():
     
 # Auto-initialize the WebSocket environment when this module is imported
 set_websocket_environment()
+print('set_websocket_environment() !!!!!!!!!')
 
 # --- 5. PRICING CONFIGURATION ---
 def _fetch_server_config():
